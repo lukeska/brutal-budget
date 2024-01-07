63f63d572b95ec4002ee9e9c2a3d53c60a3153ca
lukeska <luca.ubiali@gmail.com>
2024-01-07 16:00:56 +0100

    added sentry integration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
@@ -5,29 +6,32 @@ import eslintPlugin from "vite-plugin-eslint";
 import { watch } from "vite-plugin-watch";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.ts",
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+    plugins: [laravel({
+        input: "resources/js/app.ts",
+        refresh: true,
+    }), vue({
+        template: {
+            transformAssetUrls: {
+                base: null,
+                includeAbsolute: false,
             },
-        }),
-        watch(
-            {
-                pattern: "app/{Data,Enums}/**/*.php",
-                command: "php artisan typescript:transform",
-            },
-            {
-                pattern: "routes/*.php",
-                command: "php artisan trail:generate",
-            },
-        ),
-        //eslintPlugin(),
-    ],
+        },
+    }), //eslintPlugin(),
+    watch(
+        {
+            pattern: "app/{Data,Enums}/**/*.php",
+            command: "php artisan typescript:transform",
+        },
+        {
+            pattern: "routes/*.php",
+            command: "php artisan trail:generate",
+        },
+    ), sentryVitePlugin({
+        org: "brutal-budget",
+        project: "javascript-vue"
+    })],
+
+    build: {
+        sourcemap: true
+    }
 });
