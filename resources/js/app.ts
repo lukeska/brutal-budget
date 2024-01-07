d30e5e23c0507583fa655285b8878403cc0257a2
lukeska <luca.ubiali@gmail.com>
2024-01-07 18:59:38 +0100

    fix sentry

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -11,7 +11,7 @@ import * as Sentry from "@sentry/vue";
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 const pinia = createPinia();
 
-Sentry.init({
+/*Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
     integrations: [
         new Sentry.BrowserTracing({
@@ -28,27 +28,39 @@ Sentry.init({
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+});*/
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob("./Pages/**/*.vue")),
     setup({ el, App, props, plugin }) {
-        const vueApp = createApp({ render: () => h(App, props) })
+        const vm = createApp({ render: () => h(App, props) })
             .use(pinia)
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
 
-        vueApp.config.errorHandler = (error, vm, info) => {
-            // Log the error to Sentry
-            Sentry.captureException(error);
+        Sentry.init({
+            app: vm,
+            dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+            integrations: [
+                new Sentry.BrowserTracing({
+                    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+                    tracePropagationTargets: ["localhost"],
+                }),
+                new Sentry.Replay({
+                    maskAllText: false,
+                    blockAllMedia: false,
+                }),
+            ],
+            // Performance Monitoring
+            tracesSampleRate: 1.0, //  Capture 100% of the transactions
+            // Session Replay
+            replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+            replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        });
 
-            // Continue with default error handling
-            console.error(error, vm, info);
-        };
-
-        return vueApp;
+        return vm;
     },
     progress: {
         color: "#4B5563",
