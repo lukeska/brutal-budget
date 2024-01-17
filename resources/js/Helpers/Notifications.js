d4df40a09e91917ebc4bd1b7a3c115510d8130ad
lukeska <luca.ubiali@gmail.com>
2024-01-17 12:58:05 +0100

    expense notification test

--- resources/js/Helpers/Notifications.js
+++ resources/js/Helpers/Notifications.js
@@ -1,54 +1,47 @@
 const sendNotification = async () => {
     const registration = await navigator.serviceWorker.getRegistration();
+
     if (Notification.permission === "granted") {
-        showNotification(registration, "All good here");
+        showNotification(registration, "This is a test", "All good here");
     } else {
         if (Notification.permission !== "denied") {
             const permission = await Notification.requestPermission();
 
             if (permission === "granted") {
-                showNotification(registration, "All good here");
+                showNotification(registration, "This is a test", "All good here");
             }
         }
     }
 };
 
-const showNotification = (registration, message) => {
-    const title = "This is a test";
-
+const showNotification = (registration, title, message) => {
     const payload = {
         body: message,
+        icon: "/images/icons/icon-512x512.png",
     };
 
     if (registration && "showNotification" in registration) {
-        console.log("registration2", registration);
         registration.showNotification(title, payload);
     } else {
         new Notification(title, payload);
     }
 };
 
-const showExpenseNotification = (expense) => {
-    if (Notification.permission === "granted") {
-        // Create and display the notification
-        const notification = new Notification(expense.amount + " expense - " + expense.category.name, {
-            body: "Created by " + expense.user.name,
-            icon: "/images/icons/icon-512x512.png",
-        });
+const showExpenseNotification = async (expense) => {
+    const registration = await navigator.serviceWorker.getRegistration();
+    const title = expense.amount + " expense - " + expense.category.name;
+    const message = "Created by " + expense.user.name;
 
-        // Handle click on the notification if necessary
-        notification.onclick = () => {
-            // Handle the click event, e.g., open a relevant page
-            console.log("Notification clicked");
-        };
-    } else if (Notification.permission !== "denied") {
-        // Ask for notification permission if not granted or denied
-        Notification.requestPermission().then((permission) => {
+    if (Notification.permission === "granted") {
+        showNotification(registration, title, message);
+    } else {
+        if (Notification.permission !== "denied") {
+            const permission = await Notification.requestPermission();
+
             if (permission === "granted") {
-                // Show the notification after permission is granted
-                showNotification(notificationData);
+                showNotification(registration, title, message);
             }
-        });
+        }
     }
 };
 
