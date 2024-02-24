726ca4d9bed8957f8f0ee0d6db964c1f1d9d21ac
lukeska <luca.ubiali@gmail.com>
2024-02-24 13:49:43 +0100

    added project info in expense item

--- resources/js/Pages/Expenses/Partials/ExpenseItem.vue
+++ resources/js/Pages/Expenses/Partials/ExpenseItem.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import CategoryIcon from "@/Pages/Categories/Partials/CategoryIcon.vue";
 import { createCurrencyFormatter } from "@/Helpers/CurrencyFormatter";
-import { usePage } from "@inertiajs/vue3";
+import { usePage, Link } from "@inertiajs/vue3";
 
 let props = defineProps<{
     expense: App.Data.ExpenseData;
@@ -15,12 +15,24 @@ const currencyFormatter = createCurrencyFormatter(page.props.auth.user.currency)
 <template>
     <div class="flex w-full items-center justify-between px-3 py-2 hover:bg-neutral-50">
         <div class="w-2/3 text-left">
-            <div>
-                <slot name="prefix">
-                    <div :style="'color:' + expense.category.hex">
-                        <CategoryIcon :category="expense.category" />
-                    </div>
-                </slot>
+            <div class="flex items-center space-x-4">
+                <div>
+                    <slot name="prefix">
+                        <div :style="'color:' + expense.category.hex">
+                            <CategoryIcon :category="expense.category" />
+                        </div>
+                    </slot>
+                </div>
+                <div v-if="expense.project">
+                    <Link
+                        :href="route('projects.show', { project: expense.project.id })"
+                        class="relative flex items-center space-x-1 rounded-full pl-1 pr-2 text-sm hover:bg-gray-200">
+                        <div
+                            class="h-3 w-3 rounded-full"
+                            :style="'background:' + expense.project.hex"></div>
+                        <span>{{ expense.project.name }}</span>
+                    </Link>
+                </div>
             </div>
             <div class="text-gray-500">{{ expense.notes }}</div>
         </div>
