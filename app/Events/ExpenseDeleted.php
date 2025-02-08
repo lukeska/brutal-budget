96a0153d99cdd083d12362e95f0ac86cd565744f
lukeska <luca.ubiali@gmail.com>
2025-02-08 13:18:37 +0100

    Update events

--- app/Events/ExpenseDeleted.php
+++ app/Events/ExpenseDeleted.php
@@ -2,7 +2,9 @@
 
 namespace App\Events;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -28,7 +30,7 @@ class ExpenseDeleted implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('teams.'.$this->teamId),
+            new PrivateChannel('teams.' . $this->teamId),
         ];
     }
 }
--- app/Events/ExpenseUpdated.php
+++ app/Events/ExpenseUpdated.php
@@ -3,7 +3,9 @@
 namespace App\Events;
 
 use App\Models\Expense;
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -29,7 +31,7 @@ class ExpenseUpdated implements ShouldBroadcast
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('teams.'.$this->expense->team->id),
+            new PrivateChannel('teams.' . $this->expense->team->id),
         ];
     }
 }
