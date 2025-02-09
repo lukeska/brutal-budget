0c82b619ed9db7265b2daffbbac164a5b267590e
lukeska <luca.ubiali@gmail.com>
2025-02-09 10:31:33 +0100

    Updata Laravel Data classes

--- app/Data/Casts/CurrencyToIntCast.php
+++ app/Data/Casts/CurrencyToIntCast.php
@@ -3,11 +3,12 @@
 namespace App\Data\Casts;
 
 use Spatie\LaravelData\Casts\Cast;
+use Spatie\LaravelData\Support\Creation\CreationContext;
 use Spatie\LaravelData\Support\DataProperty;
 
 class CurrencyToIntCast implements Cast
 {
-    public function cast(DataProperty $property, mixed $value, array $context): mixed
+    public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
     {
         return round(floatval($value) * 100);
     }
