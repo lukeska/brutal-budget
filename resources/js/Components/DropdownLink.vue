743683cd1a115c9e77c60ea1c6c7047e3a6c3a8c
lukeska <luca.ubiali@gmail.com>
2025-02-15 10:41:50 +0000

    Update components

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -11,22 +11,22 @@ defineProps({
     <div>
         <button
             v-if="as == 'button'"
-            class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
-            type="submit">
+            type="submit"
+            class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800">
             <slot />
         </button>
 
         <a
             v-else-if="as == 'a'"
             :href="href"
-            class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none">
+            class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800">
             <slot />
         </a>
 
         <Link
             v-else
             :href="href"
-            class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none">
+            class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-300 dark:hover:bg-gray-800 dark:focus:bg-gray-800">
             <slot />
         </Link>
     </div>
